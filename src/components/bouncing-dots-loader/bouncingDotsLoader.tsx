97fff9ff7c3c17e6729168119093960857beb8c5
Kelvin Ward <kelvinward1010@gmail.com>
2024-07-11 16:50:11 +0700

    update prettier

---
+++ src/components/bouncing-dots-loader/bouncingDotsLoader.tsx
@@ -0,0 +1,11 @@
+import "./bouncingDotsLoader.css";
+
+export function BouncingDotsLoader() {
+    return (
+        <div className="bouncing-loader">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    );
+}
--- src/components/hoc/custom-feedback.hoc.tsx
+++ src/components/hoc/custom-feedback.hoc.tsx
@@ -1,46 +1,82 @@
-import { IComment } from "@/types/comment"
-import { IBasetListPost } from "@/types/post"
-import { Typography } from "antd"
+import { IComment } from "@/types/comment";
+import { IBasetListPost } from "@/types/post";
+import { Typography } from "antd";
 import { useTranslation } from "react-i18next";
-import './custom-feedback.hoc.css';
+import "./custom-feedback.hoc.css";
+import { BouncingDotsLoader } from "../bouncing-dots-loader/bouncingDotsLoader";
 
 const { Title } = Typography;
 
 type TitleProps = {
     children: React.ReactNode;
 };
-  
+
 function TitleCustomization({ children }: TitleProps) {
-    return <Title level={5} className="title-feedback">{children}</Title>;
+    return (
+        <>
+            <Title level={5} className="title-feedback">
+                {children}
+            </Title>
+            <BouncingDotsLoader />
+        </>
+    );
 }
 
-
 export const customConditionalFeedbackHigh =
-    (loadingFeedBack?: string, noDataFeedBack?: string, dataEmptyFeedback?: string) =>
-        (Component: React.ComponentType<{ data: IBasetListPost }>) =>
-            (props: { data: IBasetListPost }) => {
-                const { t } = useTranslation();
-                const { data, isLoading } = props.data;
+    (
+        loadingFeedBack?: string,
+        noDataFeedBack?: string,
+        dataEmptyFeedback?: string,
+    ) =>
+    (Component: React.ComponentType<{ data: IBasetListPost }>) =>
+    (props: { data: IBasetListPost }) => {
+        const { t } = useTranslation();
+        const { data, isLoading } = props.data;
 
-                if (isLoading) return <TitleCustomization>{loadingFeedBack ?? t("feedback.loading")}</TitleCustomization>
-                if (!data) return <TitleCustomization>{noDataFeedBack ?? t("feedback.nodatafeedback")}</TitleCustomization>
-                if (!data.length) return <TitleCustomization>{dataEmptyFeedback ?? t("feedback.emptydata")}</TitleCustomization>
-                return <Component {...props} />
-            }
+        if (isLoading)
+            return (
+                <TitleCustomization>
+                    {loadingFeedBack ?? t("feedback.loading")}
+                </TitleCustomization>
+            );
+        if (!data)
+            return (
+                <TitleCustomization>
+                    {noDataFeedBack ?? t("feedback.nodatafeedback")}
+                </TitleCustomization>
+            );
+        if (!data.length)
+            return (
+                <TitleCustomization>
+                    {dataEmptyFeedback ?? t("feedback.emptydata")}
+                </TitleCustomization>
+            );
+        return <Component {...props} />;
+    };
 
 type CommentProps = {
     data?: IComment[];
 };
 
-export const customConditionalCommentsFeedbackHigh = (
-    noDataFeedback?: string,
-    dataEmptyFeedback?: string,
-) => (Component: React.ComponentType<CommentProps>) => (props: CommentProps) => {
-    const { data } = props;
-    const { t } = useTranslation();
+export const customConditionalCommentsFeedbackHigh =
+    (noDataFeedback?: string, dataEmptyFeedback?: string) =>
+    (Component: React.ComponentType<CommentProps>) =>
+    (props: CommentProps) => {
+        const { data } = props;
+        const { t } = useTranslation();
 
-    if (!data) return <TitleCustomization>{noDataFeedback ?? t("feedback.nodatafeedback")}</TitleCustomization>
-    if (!data?.length) return <TitleCustomization>{dataEmptyFeedback ?? t("feedback.emptydata")}</TitleCustomization>
+        if (!data)
+            return (
+                <TitleCustomization>
+                    {noDataFeedback ?? t("feedback.nodatafeedback")}
+                </TitleCustomization>
+            );
+        if (!data?.length)
+            return (
+                <TitleCustomization>
+                    {dataEmptyFeedback ?? t("feedback.emptydata")}
+                </TitleCustomization>
+            );
 
-    return <Component {...props} />;
-};
+        return <Component {...props} />;
+    };
