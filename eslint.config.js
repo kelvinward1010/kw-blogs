57ebd127e36764d71fcebc61b5d2831ecefe607c
Kelvin Ward <kelvinward1010@gmail.com>
2024-07-11 16:25:11 +0700

    update prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,11 @@
 // eslint.config.js
 export default [
     {
+        extends: [
+            "plugin:prettier/recommended",
+            "prettier"
+        ],
+        plugins: ['prettier'],
         rules: {
             semi: "error",
             "prefer-const": "error",
