84904f898234386e42bb292c130a20d6d99f3143
Kelvin Ward <kelvinward1010@gmail.com>
2024-07-11 15:41:45 +0700

    update prettier

--- src/pages/topics/views/Topics.tsx
+++ src/pages/topics/views/Topics.tsx
@@ -4,9 +4,6 @@ import { TopicsSelect } from '../components/TopicsSelect';
 import styles from './Topics.module.scss';
 
 export function Topics() {
-
-
-
     return (
         <div className={styles.container}>
             <Row gutter={[8, 16]} wrap style={{width: 'calc(100vw - 20px)'}}>
