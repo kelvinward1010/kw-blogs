3441e2c1b2664ec789da2dcb83e5b39db7598797
Kelvin Ward <kelvinward1010@gmail.com>
2024-07-26 19:22:03 +0700

    update head

--- src/pages/home/components/head-home.tsx
+++ src/pages/home/components/head-home.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "antd";
+import { Carousel, Typography } from "antd";
 import styles from "./head-home.module.scss";
 
 const { Title } = Typography;
@@ -6,9 +6,24 @@ const { Title } = Typography;
 export function HeadHome() {
     return (
         <div className={styles.container}>
-            <Title className={styles.title}>
-                Write your content and share to everyone
-            </Title>
+            <Carousel arrows autoplay>
+                <div className={`${styles.box} ${styles.img1}`}>
+                    <Title className={styles.title}>Welcome to KW Blogs</Title>
+                </div>
+                <div className={`${styles.box} ${styles.img2}`}>
+                    <Title className={styles.title}>
+                        This is the place for you to unleash your creativity
+                    </Title>
+                </div>
+                <div className={`${styles.box} ${styles.img3}`}>
+                    <Title className={styles.title}>
+                        Write your content and share to everyone
+                    </Title>
+                </div>
+                <div className={`${styles.box} ${styles.img4}`}>
+                    <Title className={styles.title}>Always stay positive</Title>
+                </div>
+            </Carousel>
         </div>
     );
 }
